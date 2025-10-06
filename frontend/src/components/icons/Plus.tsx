7c209ea201c79e981f6f12ac94ca7b1211e9e40b
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-06 05:05:50 +0000

    Icons added

---
+++ frontend/src/components/icons/Plus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Plus() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Plus
---
+++ frontend/src/components/icons/RightArrow.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function RightArrow({ className = "" }) {
+  return (
+    <svg
+      width="5"
+      height="9"
+      viewBox="0 0 5 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g clipPath="url(#clip0_113_584)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M0.6255 0L0 0.6327L3.71745 4.50585L3.321 4.91895L3.32325 4.9167L0.02025 8.35785L0.6363 9L4.95 4.50585L0.6255 0Z"
+          fill="currentColor"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_113_584">
+          <rect width="4.95" height="9" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default RightArrow;
