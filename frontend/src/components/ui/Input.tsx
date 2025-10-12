6b05dc7e32f907c8b8695af364ab5307840e3a2b
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-12 09:31:37 +0000

    input component added css

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 interface InputProps {
   type: string;
   placeholder: string;
+  className?: string; 
 }
 
-function Input({ type, placeholder }: InputProps) {
+function Input({ type, placeholder, className = '' }: InputProps) {
   return (
     <input
       type={type}
-      className="border border-[#C9C7C7] h-16 w-[550px] rounded-4xl px-10 text-[#6b5a5a] font-neogrotesk-sc-bold"
       placeholder={placeholder}
+      className={`border border-[#C9C7C7] h-16 rounded-4xl px-10 text-[#6b5a5a] font-neogrotesk-sc-bold ${className}`}
     />
   );
 }
