59131bc81dec17fbc955316b8666fbd858b0fed2
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-14 15:50:44 +0000

    zod validation added for login and signup

---
+++ frontend/src/validate/UserValidate.tsx
@@ -0,0 +1,32 @@
+import {  z } from "zod";
+
+export const signupSchema = z.object({
+    username: z
+        .string()
+        .trim()
+        .min(4, { message: "Username must be at least 3 characters" })
+        .max(50, { message: "Username must not exceed  least 50 characters" }),
+    email: z
+        .string()
+        .trim()
+        .email({ message: "Invalid Email Format" }),
+    password: z
+        .string()
+        .min(5, { message: "Password must be at least 5 characters" })
+        .max(50, { message: "Password must not exceed least 50 character" }),
+
+    role: z.enum(["user", "admin"]).default('user'),
+    isVerified: z.boolean().default(false),
+    status: z.enum(['active', 'inactive', 'banned']).default('active'),
+})
+
+export const loginSchema = z.object({
+    email: z
+        .string()
+        .trim()
+        .email({ message: "Invalid Email Format" }),
+    password: z
+        .string()
+        .min(5, { message: "Password must be at least 5 characters" })
+        .max(50, { message: "Password must not exceed least 50 character" })
+})
