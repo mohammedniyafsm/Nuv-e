6177040fd95ece8d7c074e1bff8fb32b1f3dfc64
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-14 03:23:53 +0000

    Admin Dashbaord

---
+++ frontend/src/components/icons/Cart.tsx
@@ -0,0 +1,56 @@
+
+function Cart({ className = '' }) {
+  return (
+    <svg
+      className={className}
+      width="1024"
+      height="864"
+      viewBox="0 0 1024 864"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M544.93 341.77L590.55 387.39L677.39 300.54M302.94 343.96H452.08"
+        stroke="#2E2E2E"
+        strokeWidth="30"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M770.24 343.96H920.44L816.04 656.92H407.39L302.98 343.96L259.42 213.39H103.56"
+        stroke="#2E2E2E"
+        strokeWidth="30"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M480.59 839.11C509.668 839.11 533.24 815.538 533.24 786.46C533.24 757.382 509.668 733.81 480.59 733.81C451.512 733.81 427.94 757.382 427.94 786.46C427.94 815.538 451.512 839.11 480.59 839.11Z"
+        stroke="#2E2E2E"
+        strokeWidth="30"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M742.84 839.11C771.918 839.11 795.49 815.538 795.49 786.46C795.49 757.382 771.918 733.81 742.84 733.81C713.762 733.81 690.19 757.382 690.19 786.46C690.19 815.538 713.762 839.11 742.84 839.11Z"
+        stroke="#2E2E2E"
+        strokeWidth="30"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M770.24 343.96C770.24 431.82 699.02 503.04 611.17 503.04C523.31 503.04 452.09 431.82 452.09 343.96C452.09 256.11 523.31 184.89 611.17 184.89C699.02 184.89 770.24 256.11 770.24 343.96Z"
+        stroke="#2E2E2E"
+        strokeWidth="30"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Cart;
