ed67df2eae51e4e61362c4814c48848b42927a08
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-16 16:29:23 +0000

    Navbar updated according login or not

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,20 +2,26 @@ import { Link } from "react-router-dom";
 import Bag from "./icons/bag";
 import Person from "./icons/person";
 import Line from "./icons/line";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import type { AppDispatch, RootState } from "../app/store";
+import { fetchUser } from "../features/User/UserSlice";
 
 function Navbar() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { username } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   return (
     <div className="fixed left-0 top-0 w-full px-4 py-4 md:px-16 md:py-4">
       <div className="flex justify-between items-center">
 
         <div>
-          <img
-            className="w-12 h-6 md:w-24 md:h-12"
-            src="./public/images/logo.png"
-            alt="Logo"
-          />
+          <img className="w-12 h-6 md:w-24 md:h-12" src="/images/logo.png" alt="Logo" />
         </div>
-
 
         <div>
           <ul className="list-none flex gap-2 text-[8px] md:gap-8 md:text-xs font-neogroteskessalt-light">
@@ -26,25 +32,21 @@ function Navbar() {
           </ul>
         </div>
 
-
         <div className="bg-[#333333] w-14 h-6 md:w-30 md:h-12 rounded-[5rem] flex items-center px-2 md:px-4">
+          {username?.trim() ? (
             <>
-              <Link to="/cart">
-                <Bag className="w-5 h-3 md:w-8 md:h-6 text-white" />
-              </Link>
+              <Link to="/cart"><Bag className="w-5 h-3 md:w-8 md:h-6 text-white" /></Link>
               <Line className="w-2 h-2.5 md:w-8 md:h-6 md:mr-1 text-white" />
-              <Link to="/account">
-                <Person className="w-4 h-2.5 md:w-5 md:h-6 text-white" />
-              </Link>
+              <Link to="/account"><Person className="w-4 h-2.5 md:w-5 md:h-6 text-white" /></Link>
             </>
-      
-            {/* <Link to="/login" className="  text-white">
-              <span className="text-md font-neogrotesk-regular pl-6">Login</span>
-            </Link> */}
+          ) : (
+            <Link to="/login"><span className="text-white pl-6 font-neogrotesk-regular">Login</span></Link>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
 
 export default Navbar;
