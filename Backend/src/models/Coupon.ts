8c6f9fdeedc5fe1f44ff61bbd031821ff2a4b33e
Mohammed Niyaf <mohammedniyafpc@gmail.com>
2025-10-16 16:25:19 +0000

    added subtotal and discount and total amount cart model

--- Backend/src/models/Coupon.ts
+++ Backend/src/models/Coupon.ts
@@ -1,52 +1,33 @@
-import { model, Schema, Types } from "mongoose";
+import { model, Schema, Types, Document } from "mongoose";
 
-
-interface ICoupon extends Document {
-    code : string,
-    discountAmount : number,
-    discountPercentage? : number,
-    minCartAmount? : number,
-    maxUsagePerUser : number,
-    expiryDate : Date,
-    userUsed : {
-        userId : Types.ObjectId,
-        timesUsed : number,
-    }[];
+interface IUserUsed {
+  userId: Types.ObjectId;
+  timesUsed: number;
 }
 
-const couponSchema = new Schema <ICoupon>({
-    code : {
-        type : String,
-        required : true,
-    },
-    discountAmount : {
-        type : Number,
-        required : true,
-    },
-    discountPercentage : {
-        type : Number,
-    },
-    minCartAmount : {
-        type : Number,
-    },
-    maxUsagePerUser : {
-        type : Number,
-    },
-    expiryDate : {
-        type : Date
-    },
-    userUsed : [
-        {
-            userId : {
-                type : Schema.Types.ObjectId,
-                ref : 'User'
-            },
-            timeUsed : {
-                type : Number,
-                default : 0
-            }
-        }
-    ]
-})
+export interface ICoupon extends Document {
+  code: string;
+  discountAmount: number;
+  discountPercentage?: number;
+  minCartAmount?: number;
+  maxUsagePerUser: number;
+  expiryDate?: Date;
+  userUsed: IUserUsed[];
+}
 
-export const Coupon = model <ICoupon>('Coupon',couponSchema);
+const couponSchema = new Schema<ICoupon>({
+  code: { type: String, required: true },
+  discountAmount: { type: Number, required: true },
+  discountPercentage: { type: Number },
+  minCartAmount: { type: Number },
+  maxUsagePerUser: { type: Number, required: true },
+  expiryDate: { type: Date },
+  userUsed: [
+    {
+      userId: { type: Schema.Types.ObjectId, ref: "User" },
+      timesUsed: { type: Number, default: 0 },
+    },
+  ],
+});
+
+export const Coupon = model<ICoupon>("Coupon", couponSchema);
